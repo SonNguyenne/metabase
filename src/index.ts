89a9f9b1abcc4e1135e54f1819387f5f4267dd2d
SonNguyen <ts29032001@gmail.com>
2023-03-29 15:50:07 +0700

    export service

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './keys';
-export * from './test.component';
-export * from './types';
+export * from "./keys";
+export * from "./test.component";
+export * from "./types";
+export * from "./services";
